3d33594c1d593272eb711d722e86c8c87f27395b
Brett Lazarine <jblazarine@gmail.com>
2023-01-25 22:07:36 -0600

    complete exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("What's your favorite school subject: ");
+            var favSubject = Console.ReadLine();
+            switch (favSubject.ToLower())
+            {
+                case "potions":
+                    Console.WriteLine("Snape approves.");
+                    break;
+                case "flying":
+                    Console.WriteLine("Don't fall off your broomstick.");
+                    break;
+                case "herbology":
+                    Console.WriteLine("Those shrieking plants sure are loud.");
+                    break;
+                case "defence against the dark arts":
+                    Console.WriteLine("Lots of professors to pick from.");
+                    break;
+                case "transfiguration":
+                    Console.WriteLine("Have fun turning into a cat.");
+                    break;
+                default:
+                    Console.WriteLine("Wait, is this not Hogwarts?");
+                    break;
+            }
         }
     }
 }
